c55ed17e4272c2d1366486e818c168b5781e9c81
Nikhil Bhatia <nik.bhatia.13@gmail.com>
2022-12-25 09:34:34 +0530

    initial

---
+++ src/main/java/com/example/demo/SampleRestControllerExample.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleRestControllerExample {
+
+	 @GetMapping("/hello")
+	    // Easy method just to print encouraging and consoling words
+	    public String hello() {
+	        return "Hello Geek, this is a simple hello message to take care and have a nice day.";
+	    }
+	     
+	    @GetMapping("/greet")
+	    //Easy method just to print greeting message by saying spring-based applications
+	    public String greet() {
+	        return "Hello Geek, Fast and easy development can be possible on Spring-based applications by reducing source code;.";
+	    }
+}
